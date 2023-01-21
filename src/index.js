61592abb09c758c42ecc9309039f7d2e6da79b26
jrpinzon88 <jrpinzon88@gmail.com>
2023-01-21 18:46:34 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,34 @@
  **/
 
 console.log('Happy hacking :)')
+
+const url = "https://platzi-avo.vercel.app/api/avo";
+const appNode = document.querySelector('#app')
+
+window.fetch(url)
+.then((response) => response.json())
+.then((respuestaJson) => {
+    respuestaJson.data.forEach(item => { 
+        //Crear Imagen
+        const img = document.createElement('img');
+        img.setAttribute('src', `https://platzi-avo.vercel.app${item.image}`)
+        
+        //Crear Titulo
+        const titulo = document.createElement('h2');
+        titulo.innerText = item.name
+        titulo.className = "text-xl";
+        
+        //Crear Precio
+        const price = document.createElement('p');
+        price.innerText = `$ ${item.price}`
+        
+
+        const container = document.createElement('div');
+        container.className = "w-80"
+
+        container.append(img, titulo, price)
+        appNode.appendChild(container);
+
+    })}
+)
+    
